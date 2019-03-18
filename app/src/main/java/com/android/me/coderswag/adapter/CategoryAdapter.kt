f3b1b193b5b0bbe83e861aea9a1a1e1eeda875df
Abhimanyu Datta <a.datta47@gmail.com>
2019-03-19 00:02:40 +0530

    Added view holder for category adapter

--- app/src/main/java/com/android/me/coderswag/adapter/CategoryAdapter.kt
+++ app/src/main/java/com/android/me/coderswag/adapter/CategoryAdapter.kt
@@ -16,18 +16,26 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
     val context = context
     val categories = categories
 
+    // called every time view is created, list is scrolled
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
-
-        val categoryImage: ImageView = categoryView.categoryImg
-        val categoryName: TextView = categoryView.categoryName
-
+        var categoryView: View
+        val holder: ViewHolder
+        if (null == convertView) {
+            // inflate view once, for the first time
+            categoryView = LayoutInflater.from(context).inflate(R.layout.category_list_item, null)
+            holder = ViewHolder()
+            holder.categoryImage = categoryView.categoryImg
+            holder.categoryName = categoryView.categoryName
+            categoryView.tag = holder
+        } else {
+            // use previous holder
+            holder = convertView.tag as ViewHolder
+            categoryView = convertView
+        }
         val category = categories[position]
-        categoryName.text = category.title
-
+        holder.categoryName?.text = category.title
         val resourceId = context.resources.getIdentifier(category.image, "drawable", context.packageName)
-        categoryImage.setImageResource(resourceId)
-
+        holder.categoryImage?.setImageResource(resourceId)
         return categoryView
     }
 
@@ -41,6 +49,12 @@ class CategoryAdapter(context: Context, categories: List<Category>) : BaseAdapte
 
     override fun getCount(): Int {
         return categories.count()
+    }
+
+    // view holder to hold an recycle views
+    private class ViewHolder {
+        var categoryImage: ImageView? = null
+        var categoryName: TextView? = null
     }
 
 }
