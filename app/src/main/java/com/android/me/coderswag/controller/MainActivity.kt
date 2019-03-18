e2bc99d115960787a10089dd2e6263318238aa77
Abhimanyu Datta <a.datta47@gmail.com>
2019-03-18 23:23:00 +0530

    added category layout

--- app/src/main/java/com/android/me/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/android/me/coderswag/controller/MainActivity.kt
@@ -2,12 +2,23 @@ package com.android.me.coderswag.controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import com.android.me.coderswag.R
+import com.android.me.coderswag.model.Category
+import com.android.me.coderswag.service.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, DataService.categories)
+
+        categoryListView.adapter = adapter
+
     }
 }
